74881f06b7dd913e8dbf1d403a78d24b65dec8f4
hansani111 <hansanideshmukh894@gmail.com>
2023-01-17 22:25:45 +0530

    added: single node to linkedlist

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> Linkedlist1 = new LinkedList<>();
+        Linkedlist1.push(70);
+    }
+}
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Node<E> {
+    E data;
+    Node<E> next;
+
+    /* Whenever we will create the object of node that time 'data' will be assigned & next value will be null */
+    public Node(E data) {
+        this.data = data;
+    }
+}
